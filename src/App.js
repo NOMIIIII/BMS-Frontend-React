bf844d31881be35bbe141c70bf3da45e307e969e
Nauman Ahmed <naumanahmed449@gmail.com>
2022-11-09 01:01:40 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
+        <a class="navbar-brand" href="#">Books Management Systems</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample10"
+          aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample10">
+          <ul class="navbar-nav ">
+            <li class="nav-item">
+              <a class="btn btn-outline-warning text-dark mr-2" routerLink="/">Home</a>
+            </li>
+
+            <li  class="nav-item">
+              <a class="btn btn-outline-warning text-dark mr-2" >Add Book</a>
+            </li>
+            <li  class="nav-item">
+              <a class="btn btn-outline-warning text-dark mr-2" >Book List</a>
+            </li>
+          
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
