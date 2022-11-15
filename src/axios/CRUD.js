d374f4995d5e6b15e980cd7c88fbcc393ccfeb8a
Nauman Ahmed <naumanahmed449@gmail.com>
2022-11-15 12:27:17 +0100

    deployment

--- src/axios/CRUD.js
+++ src/axios/CRUD.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:3000"
+// export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://bms-backend-node.herokuapp.com"
 
 
 export const getBooks = async (data) => {
